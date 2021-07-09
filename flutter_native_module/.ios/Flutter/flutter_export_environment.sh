6967cb9bc4d3688358fd6ba66a78527bfbc70d92
TwistedFate <783919313@qq.com>
2021-07-09 18:04:22 +0800

    删除多余demo

---
+++ flutter_native_module/.ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Volumes/TwistedFateHD/Environment/flutter/flutter_new"
+export "FLUTTER_APPLICATION_PATH=/Volumes/TwistedFateHD/GitHub/BlogDemo-iOS/flutter_native_module"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
+export "FLUTTER_TARGET=lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=false"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=.packages"
