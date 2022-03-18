d5039a07b5fa6c2f034f57b47c3e09e763217276
TwistedFate <783919313@qq.com>
2022-03-18 18:01:56 +0800

    runtimedemo

--- RuntimeDemo/RuntimeDemo/Person.m
+++ RuntimeDemo/RuntimeDemo/Person.m
@@ -10,7 +10,14 @@
 @interface Person ()
 
 {
-    char _constant;
+    struct {
+        /// 位域只占一个字节
+        char tall: 1;
+        char rich: 1;
+        char handsome: 1;
+    } _constant; // 0b0000 0001
+    
+//    char _constant;
 }
 
 @end
@@ -24,51 +31,57 @@
 
 - (instancetype)init {
     if (self = [super init]) {
-        _constant = 0b00000001;
+//        _constant = 0b00000001;
     }
     return self;
 }
 
 
 - (BOOL)isRich {
-    return !!(_constant & kRichMask);
+    return _constant.rich;
+//    return !!(_constant & kRichMask);
 }
 
 - (void)setRich:(BOOL)rich {
-    if (rich) {
-//        _constant = _constant ! kRichMask;
-        _constant |= kRichMask;
-    } else {
-        // ~每位取反
-        _constant &= ~kRichMask;
-    }
+//    if (rich) {
+////        _constant = _constant ! kRichMask;
+//        _constant |= kRichMask;
+//    } else {
+//        // ~每位取反
+//        _constant &= ~kRichMask;
+//    }
+    _constant.rich = rich;
 }
 
 - (BOOL)isTall {
-    return !!(_constant & kTallMask);
+//    return !!(_constant & kTallMask);
+    return _constant.tall;
 }
 
 - (void)setTall:(BOOL)tall {
-    if (tall) {
-//        _constant = _constant ! kRichMask;
-        _constant |= kTallMask;
-    } else {
-        // ~每位取反
-        _constant &= ~kTallMask;
-    }
+//    if (tall) {
+////        _constant = _constant ! kRichMask;
+//        _constant |= kTallMask;
+//    } else {
+//        // ~每位取反
+//        _constant &= ~kTallMask;
+//    }
+    _constant.tall = tall;
 }
 
 - (BOOL)isHandsome {
-    return !!(_constant & kHandsomeMask);
+//    return !!(_constant & kHandsomeMask);
+    return _constant.handsome;
 }
 
 - (void)setHandsome:(BOOL)handsome {
-    if (handsome) {
-//        _constant = _constant ! kRichMask;
-        _constant |= kHandsomeMask;
-    } else {
-        // ~每位取反
-        _constant &= ~kHandsomeMask;
-    }
+//    if (handsome) {
+////        _constant = _constant ! kRichMask;
+//        _constant |= kHandsomeMask;
+//    } else {
+//        // ~每位取反
+//        _constant &= ~kHandsomeMask;
+//    }
+    _constant.handsome = handsome;
 }
 @end
