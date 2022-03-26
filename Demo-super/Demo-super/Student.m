6c6a5abd74892ac8ce1b74cd01e59b32f3078efa
zpfate <783919313@qq.com>
2022-03-26 22:11:27 +0800

    isKindOfcClass、isMemberOfClass

--- Demo-super/Demo-super/Student.m
+++ Demo-super/Demo-super/Student.m
@@ -14,11 +14,18 @@
     self = [super init];
     if (self) {
         
-        NSLog(@"%@", [self class]);
-        NSLog(@"%@", [self superclass]);
+        /** super
+         super调用的是objc_msgSendSuper
+         该方法需要传入一个objc_super的结构体
+         结构体objc有两个参数:
+                receiver: 方法接收者
+                superClass: 从父类寻找方法的实现
+         */
+        NSLog(@"%@", [self class]); // Student
+        NSLog(@"%@", [self superclass]); // Person
         NSLog(@"---------------------");
-        NSLog(@"%@", [super class]);
-        NSLog(@"%@", [super superclass]);
+        NSLog(@"%@", [super class]); // Student
+        NSLog(@"%@", [super superclass]); // Person
 
         
     }
--- Demo-super/Demo-super/main.m
+++ Demo-super/Demo-super/main.m
@@ -7,7 +7,7 @@
 
 #import <Foundation/Foundation.h>
 #import "Student.h"
-
+#import "Person.h"
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         // insert code here...
@@ -15,6 +15,11 @@ int main(int argc, const char * argv[]) {
         Student *student = [[Student alloc] init];
         
         
+        
+        NSLog(@"%d", [[NSObject class] isKindOfClass:[NSObject class]]);
+        NSLog(@"%d", [[NSObject class] isMemberOfClass:[NSObject class]]);
+        NSLog(@"%d", [[Person class] isKindOfClass:[Person class]]);
+        NSLog(@"%d", [[Person class] isMemberOfClass:[Person class]]);
     }
     return 0;
 }
